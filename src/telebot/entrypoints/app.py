4a4360b7767a0940895c83054a0238a06a21d680
binchoi <binchoi16@gmail.com>
2023-11-01 14:22:53 +0900

    Add old changes

--- src/telebot/entrypoints/app.py
+++ src/telebot/entrypoints/app.py
@@ -1,25 +1,64 @@
+
 import logging
 import os
+import random
+import string
 
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 
+logger = logging.getLogger(__name__)
 
-# async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+def start(update: Update, context: CallbackContext):
+    user = update.effective_user
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="""
+        Welcome to _Save That Spine_ <br /> To create a new race, send _create_. 
+        To join an existing race, send _join <race-code>_ (include the actual 
+        race code you have received from your fellow racer)
+        """,
+        parse_mode="markdown",
+    )
+
+
+def start_race(update: Update, context: CallbackContext):
+    user = update.effective_user
+
+    # generate random race_code
+    letters = string.ascii_lowercase
+    race_code = ''.join(random.choice(letters) for i in range(6))
+
+
+
+# def echo(update: Update, context: CallbackContext):
+#     message = f"You say, _{update.message.text}_, I say *cool, idc*"
+#     context.bot.send_message(
+#         chat_id=update.effective_chat.id,
+#         text=message,
+#         parse_mode="markdown",
+#     )
 
 
 if __name__ == '__main__':
-    TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")  # fix this
-    print(TOKEN)
-    # application = ApplicationBuilder().token('TOKEN').build()
-    #
-    # start_handler = CommandHandler('start', start)
-    # application.add_handler(start_handler)
-    #
-    # application.run_polling()
+    # create the updater
+    TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+    updater = Updater(TELEGRAM_BOT_TOKEN)
+
+    # get dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # register handlers
+    dispatcher.add_handler(CommandHandler('start', start))
+    # dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # start the bot
+    updater.start_polling()
+
+    updater.idle()
+
